15e79d88baf4e9be4f02ba85245da83bbc13b48c
unknown <ashikrobi@gmail.com>
2021-05-30 16:20:03 +0600

    Completed talkbuzz component with style

--- src/components/Talkbuzz/Talkbuzz.js
+++ src/components/Talkbuzz/Talkbuzz.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Talkbuzz.css";
 import Chat from "../Chat/Chat";
+import Sidebar from "../Sidebar/Sidebar";
 
 function Talkbuzz() {
   return (
     <div className='talkbuzz'>
+      <Sidebar />
       <Chat />
     </div>
   );
