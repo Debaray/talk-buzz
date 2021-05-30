be2bb0fcbd4dd1501e9a9de2d23141272afa7ffc
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-30 17:31:12 +0800

    using chatSlice component

--- src/components/SidebarChat/SidebarChat.js
+++ src/components/SidebarChat/SidebarChat.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./SidebarChat.css";
-
+import { setChat } from '../../features/chatSlice';
+import db from '../../config/firebase.config';
+import { useDispatch } from 'react-redux';
 
 function SidebarChat() {
+
+    const dispatch = useDispatch();
+    const [chatInfo, setChatInfo] = useState([]);
     return (
         <div className="sidebarChat">
             <h2>this is sidebar chat</h2>
