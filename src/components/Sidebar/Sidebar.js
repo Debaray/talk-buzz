9ba6b7417ec7ebce5a9467555844bd995c424a97
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-30 16:49:53 +0800

    added sidebar chats section

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,18 +1,33 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core'
+import { RateReviewOutlined } from '@material-ui/icons'
 import React from 'react'
 import "./Sidebar.css"
 
 function Sidebar() {
+
+
     return (
         <div className="sidebar">
             <div className="sidebar_header">
-                <Avatar />
+                <Avatar
+                    onClick={() => auth.signOut()}
+                    src={user.photo} className="sidebar_avatar"
+
+                />
                 <div className="sidebar_input">
                     <Search />
                     <input type="text" placeholder="Search" />
                 </div>
-
+                <IconButton variant="outlined" className="sidebar_inputButton">
+                    <RateReviewOutlined onClick={addChat} />
+                </IconButton>
             </div>
+            <div className="sidebar_chats">
+                {chats.map(({ id, data: { chatName } }) => (
+                    < SidebarChat key={id} id={id} chatName={chatName} />
+                ))}
+            </div>
+
         </div >
     )
 }
