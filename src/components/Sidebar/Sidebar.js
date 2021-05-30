d55bfaf42e5e0dba08d1e46e9e4b28a239648000
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-30 16:37:02 +0800

    added sidebar css file

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./Sidebar.css"
 
 function Sidebar() {
     return (
-        <div>
+        <div className="sidebar">
             <h1>this is sidebar</h1>
-        </div>
+        </div >
     )
 }
 
