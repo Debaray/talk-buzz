c4f1dc632a56516c6895bf10a1d2389cededc6d2
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-30 16:08:29 +0800

    added sidebarChat component

---
+++ src/components/SidebarChat/SidebarChat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SidebarChat() {
+    return (
+        <div>
+            <h2>this is sidebar chat</h2>
+        </div>
+    )
+}
+
+export default SidebarChat
+
