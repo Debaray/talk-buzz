097c5c2f4bf7fe92d2bff9e0e0bda5d39567bd36
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-05-30 18:02:37 +0800

    using forwardRdf

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -5,13 +5,17 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../../features/userSlice';
 import { Avatar } from '@material-ui/core';
 
-function Message() {
+const Message = forwardRef(({
+    id,
+    contents: { timestamp, displayName, email, message, photo, uid },
+}, ref) => {
+    const user = useSelector(selectUser);
     return (
         <div>
             <Avatar className="message_photo" src={photo} />
         </div>
     )
-}
+})
 
 export default Message
 
