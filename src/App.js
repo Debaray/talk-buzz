0ec5a0816e2b8c7b4fdd0ca6ced37bf4a4981442
Debashis Ray <debaray14@gmail.com>
2021-05-30 17:11:07 +0600

    added all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './App.css';
-
+import "./App.css";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {selectUser, login, logout} from "./features/userSlice";
+import Login from "./components/Login/Login";
+import {auth} from "./config/firebase.config";
+import Talkbuzz from "./components/Talkbuzz/Talkbuzz";
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user is logged in
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
   return (
-    <div>
-    </div>
+    <div>{user ? <Talkbuzz /> : <Login />}</div>
   );
 }
 
